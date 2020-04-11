49b98598efeb53e75e86c3984500c0521633bb12
Samratkunwar <samratkunwar12@gmail.com>
2020-04-11 23:34:10 +1000

    profile start

--- myApp/src/app/profile/profile.page.ts
+++ myApp/src/app/profile/profile.page.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
+  Cname : string = "Sizzler cafe";
+  Cemail: string = "siz@s.com";
+  Ctype: string  = "Restaurant";
+
+  items = [
+    {name:"item 1", price:20 , quantity: 100 },
+    {name:"item 2", price:20 , quantity: 100 },
+    {name:"item 3", price:20 , quantity: 100 }
+  ]; 
   constructor() { }
 
   ngOnInit() {
