6f84faa1746761d0c7d4ec27e86e91d60963825f
Samratkunwar <samratkunwar12@gmail.com>
2020-04-15 23:52:46 +1000

    version: 2.1

--- myApp/src/app/cart/cart.page.ts
+++ myApp/src/app/cart/cart.page.ts
@@ -12,14 +12,20 @@ export class CartPage implements OnInit {
 
   cart = [
     { organization:"Restaurant 1", item_name: "item 1", item_price: 20, item_quantity: 20, total_amount: 400},
-    { organization:"Restaurant 1", item_name: "item 1", item_price: 20, item_quantity: 20, total_amount: 400}
+    { organization:"Restaurant 1", item_name: "item 2", item_price: 20, item_quantity: 25, total_amount: 500}
   ]
   
+  total_amount: number = 0;
 
   ngOnInit() {
+    for (let i = 0; i < this.cart.length; i++) {
+      this.total_amount = this.total_amount + this.cart[i].total_amount;
+      
+    } 
   }
 
   confirmOrder() {
+    this.cart = [];
     alert('Order Confirmed');
   }
 
